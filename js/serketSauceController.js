858286f07bdd44542dfa55dc005c679cd9a09771
sanjsanj <sanjsanj@hotmail.com>
2015-10-29 10:57:47 +0000

    http callback in controller

---
+++ js/serketSauceController.js
@@ -0,0 +1,8 @@
+app.controller('SerketSauceController', ['$http', function($http) {
+  var self = this;
+  self.data = [];
+
+  $http.get('http://www.serket.uk/pharmacies/pharmacylist-format').success(function(data) {
+    self.data = data;
+  });
+}]);

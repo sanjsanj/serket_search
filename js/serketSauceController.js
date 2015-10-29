8c6f11df847dfd35588b4956ba579c5f66bcb160
Emily Carson <emilyjanecarson@gmail.com>
2015-10-29 13:42:42 +0000

    datatables now showing on view

--- js/serketSauceController.js
+++ js/serketSauceController.js
@@ -2,7 +2,11 @@ app.controller('SerketSauceController', ['$http', function($http) {
   var self = this;
   self.data = [];
 
+  $(document).ready( function () {
+    $('#table_id').DataTable();
+} );
+
   $http.get('http://www.serket.uk/pharmacies/pharmacylist-format').success(function(data) {
-    self.data = data;
+    self.data = data.data;
   });
 }]);
